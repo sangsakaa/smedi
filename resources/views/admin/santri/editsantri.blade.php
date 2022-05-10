6478760673d6468db9215c37a672e52a1d2b3940
sangsakaa <zainuri717@gmail.com>
2022-05-10 18:28:14 +0700

    form edit santri

--- resources/views/admin/santri/editsantri.blade.php
+++ resources/views/admin/santri/editsantri.blade.php
@@ -9,63 +9,60 @@
             <form action="/santri/{{$santri->id}}" method="post">
                 @csrf
                 @method('patch')
-                <div class=" w-full flex">
+                <div class=" grid grid-cols-1 sm:grid-cols-1 w-full sm:w-1/4">
                     <label for="" class=" capitalize mr-2"> Nama Lengkap</label>
-                </div>
-                <input name="nama_santri" type="text" class="text-green-800 py-1 px-1 w-1/4  rounded-md"
-                    placeholder=" Masuk nama Lengkap " value="{{$santri->nama_santri}}" required>
-                <div class=" w-full flex">
+                    <input name="nama_santri" type="text"
+                        class=" border border-green-800 text-green-800 py-1 px-2   rounded-md"
+                        placeholder=" Masuk nama Lengkap " value="{{$santri->nama_santri}}" required>
                     <label for="" class=" mr-2"> Jenis Kelamin</label>
-                </div>
-                <select name="jenis_kelamin" id="" class="text-green-800 py-1 px-2 w-1/4 rounded-md  " required>
-                    <option value="">Pilih Jenis Kelamin</option>
-                    <option {{old('jenis_kelamin',$santri->jenis_kelamin)=="L"? 'selected':''}} value="L">
-                        Laki Laki</option>
-                    <option {{old('jenis_kelamin',$santri->jenis_kelamin)=="P"? 'selected':''}} value="P">
-                        Perempuan</option>
-                </select>
-                <div class=" w-full flex">
+                    <select name="jenis_kelamin" id=""
+                        class="border border-green-800  text-green-800 py-1 px-2  rounded-md  " required>
+                        <option value="">Pilih Jenis Kelamin</option>
+                        <option {{old('jenis_kelamin',$santri->jenis_kelamin)=="L"? 'selected':''}} value="L">
+                            Laki Laki</option>
+                        <option {{old('jenis_kelamin',$santri->jenis_kelamin)=="P"? 'selected':''}} value="P">
+                            Perempuan</option>
+                    </select>
                     <label for="" class=" mr-2">Agama </label>
-                </div>
-                <select name="agama" id="" class="text-green-800 py-1 px-2 w-1/4 rounded-md" value="{{$santri->agama}}"
-                    required>
-                    <option {{old('agama',$santri->agama)=="Islam"? 'selected':''}} value="Islam">
-                        Islam</option>
-
-                </select>
-                <div class=" w-full flex">
+                    <select name="agama" id="" class="border border-green-800 text-green-800 py-1 px-2  rounded-md"
+                        value="{{$santri->agama}}" required>
+                        <option {{old('agama',$santri->agama)=="Islam"? 'selected':''}} value="Islam">
+                            Islam</option>
+                    </select>
                     <label for="" class=" mr-2"> Tempat Lahir</label>
-                </div>
-                <input name="tempat_lahir" type="text" class=" text-green-800 py-1 px-1 w-1/4 rounded-md"
-                    placeholder=" Masuk tempat lahir " value="{{$santri->tempat_lahir}}" required> <br>
-                <div class=" w-full flex">
+                    <input name="tempat_lahir" type="text"
+                        class=" border border-green-800 text-green-800 py-1 px-2  rounded-md"
+                        placeholder=" Masuk tempat lahir " value="{{$santri->tempat_lahir}}" required>
                     <label for="" class=" mr-2"> Tanggal Lahir </label>
-                </div>
-                <input name="tanggal_lahir" type="date" class=" text-green-800 py-1 px-1 w-1/4  rounded-md"
-                    placeholder=" Masuk tempat lahir " value="{{$santri->tanggal_lahir}}" required>
-                <div class=" w-full flex">
+                    <input name="tanggal_lahir" type="date"
+                        class="border border-green-800 text-green-800 py-1 px-2   rounded-md"
+                        placeholder=" Masuk tempat lahir " value="{{$santri->tanggal_lahir}}" required>
                     <label for="">Tanggal Masuk</label>
-                </div>
-                <input name="tanggal_masuk" type="date" class=" text-green-800 py-1 px-1 w-1/4  rounded-md"
-                    placeholder=" Masuk tempat lahir " value="{{$santri->tanggal_masuk}}" required>
-                <br>
-                <div class=" w-full flex">
+                    <input name="tanggal_masuk" type="date"
+                        class="border border-green-800 text-green-800 py-1 px-2   rounded-md"
+                        placeholder=" Masuk tempat lahir " value="{{$santri->tanggal_masuk}}" required>
                     <label for="" class=" mr-2"> Nama Ibu </label>
-                </div>
-                <input name="nama_ibu" type="text" class=" text-green-800 py-1 px-1 w-1/4  rounded-md"
-                    placeholder=" Masuk nama ibu " value="{{$santri->nama_ibu}}" required>
-                <div class=" w-full flex">
+                    <input name="nama_ibu" type="text"
+                        class="border border-green-800 text-green-800 py-1 px-2   rounded-md"
+                        placeholder=" Masuk nama ibu " value="{{$santri->nama_ibu}}" required>
                     <label for="" class=" mr-2"> Asal Kota </label>
-                </div>
-                <input name="asal_kota" type="text" class="text-green-800 py-1 px-1 w-1/4  rounded-md"
-                    placeholder=" Masuk nama Lengkap " value="{{$santri->asal_kota}}" required>
-                <div class=" w-full flex">
-                    <div class=" w-full flex">
-                        <button class=" mt-1  bg-green-800 py-1 px-1 w-1/4 d-block  mb-2 text-white rounded-md">
+                    <input name="asal_kota" type="text"
+                        class="border border-green-800 text-green-800 py-1 px-2   rounded-md"
+                        placeholder=" Masuk nama Lengkap " value="{{$santri->asal_kota}}" required>
+                    <div class=" grid grid-cols-2  gap-2 ">
+                        <button class=" mt-1  bg-green-800 py-1 px-1    mb-2 text-white rounded-md">
                             Update
                         </button>
+                        <a href="/santri">
+                            <button class=" mt-1  bg-green-800 py-1 px-1    mb-2 text-white rounded-md">
+                                Kembali
+                            </button>
+                        </a>
                     </div>
-            </form>
+                </div>
         </div>
+    </div>
+    </form>
+    </div>
     </div>
 </x-app-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col overflow-y-auto md:flex-row">
         <div class="h-32 md:h-auto md:w-1/2">
             <img aria-hidden="true" class="object-cover w-full h-full"
-                 src="{{ asset('images/create-account-office.jpeg') }}" alt="Office"/>
+                src="{{ asset('images/create-account-office.jpeg') }}" alt="Office" />
         </div>
 
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
@@ -11,44 +11,30 @@
                     Create account
                 </h1>
 
-                <x-auth-validation-errors :errors="$errors"/>
+                <x-auth-validation-errors :errors="$errors" />
 
                 <form method="POST" action="{{ route('register') }}">
-                @csrf
+                    @csrf
 
                     <div class="mt-4">
-                        <x-label for="name" :value="__('Name')"/>
-                        <x-input type="text"
-                                 id="name"
-                                 name="name"
-                                 class="block w-full"
-                                 value="{{ old('name') }}"
-                                 required
-                                 autofocus/>
+                        <x-label for="name" :value="__('Name')" />
+                        <x-input type="text" id="name" name="name" class="block w-full" value="{{ old('name') }}"
+                            required autofocus />
                     </div>
 
                     <div class="mt-4">
-                        <x-label for="email" :value="__('Email')"/>
-                        <x-input name="email"
-                                 type="email"
-                                 class="block w-full"
-                                 value="{{ old('email') }}"/>
+                        <x-label for="email" :value="__('Email')" />
+                        <x-input name="email" type="email" class="block w-full" value="{{ old('email') }}" />
                     </div>
 
                     <div class="mt-4">
-                        <x-label for="password" :value="__('Password')"/>
-                        <x-input type="password"
-                                 name="password"
-                                 class="block w-full"
-                                 required/>
+                        <x-label for="password" :value="__('Password')" />
+                        <x-input type="password" name="password" class="block w-full" required />
                     </div>
 
                     <div class="mt-4">
-                        <x-label id="password_confirmation" :value="__('Confirm Password')"/>
-                        <x-input type="password"
-                                 name="password_confirmation"
-                                 class="block w-full"
-                                 required/>
+                        <x-label id="password_confirmation" :value="__('Confirm Password')" />
+                        <x-input type="password" name="password_confirmation" class="block w-full" required />
                     </div>
 
                     <div class="mt-4">
@@ -58,11 +44,11 @@
                     </div>
                 </form>
 
-                <hr class="my-8"/>
+                <hr class="my-8" />
 
                 <p class="mt-4">
                     <a class="text-sm font-medium text-primary-600 hover:underline"
-                       href="{{ route('login') }}">{{ __('Already registered?') }}</a>
+                        href="{{ route('login') }}">{{ __('Already registered?') }}</a>
                 </p>
             </div>
         </div>
