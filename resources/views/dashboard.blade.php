e92464eb127d522d5381879264183a4bddad0cb3
sangsakaa <zainuri717@gmail.com>
2022-05-11 21:24:25 +0700

    gray purple2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <div class=" grid w-full gap-2  grid-cols-1 sm:grid-cols-4 sm:text-sm">
         <!-- str -->
         <div class=" grid grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2 bg-gray-200 rounded-lg shadow-md">
+            <div class="inline-flex overflow-hidden mb-2 bg-purple-100 rounded-lg shadow-md">
                 <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
@@ -16,7 +16,7 @@
                         </div>
                         <div class=" grid  grid-cols-1    ">
                             <div
-                                class=" bg-blue-400 lg:px-2 lg:py-1    px-8 py-2   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
+                                class=" bg-purple-800 lg:px-2 lg:py-1    px-8 py-2   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor"
                                     class="bi bi-people-fill text-purple-800 " viewBox="0 0 16 16">
                                     <path
@@ -35,8 +35,8 @@
         <!-- end -->
         <!-- str -->
         <div class=" grid grid-cols-1 sm:grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2  bg-gray-150 rounded-lg shadow-md">
-                <div class="flex justify-center items-center w-1 bg-blue-800">
+            <div class="inline-flex overflow-hidden mb-2  bg-purple-100 rounded-lg shadow-md">
+                <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
                     <div class=" grid grid-cols-2 gap-2 w-full">
@@ -46,7 +46,7 @@
                         </div>
                         <div class=" grid  grid-cols-1    ">
                             <div
-                                class=" bg-blue-800 lg:px-2 lg:py-1  px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
+                                class=" bg-purple-800 lg:px-2 lg:py-1  px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor"
                                     class="bi bi-people-fill text-purple-800 " viewBox="0 0 16 16">
                                     <path
@@ -65,8 +65,8 @@
         <!-- end -->
         <!-- str -->
         <div class=" grid grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2  bg-gray-150 rounded-lg shadow-md">
-                <div class="flex justify-center items-center w-1 bg-blue-800">
+            <div class="inline-flex overflow-hidden mb-2  bg-purple-100 rounded-lg shadow-md">
+                <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
                     <div class=" grid grid-cols-2 gap-2 w-full">
@@ -77,7 +77,7 @@
                         </div>
                         <div class=" grid  grid-cols-1    ">
                             <div
-                                class=" bg-purple-200 lg:px-2 lg:py-1 px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
+                                class=" bg-purple-100 lg:px-2 lg:py-1 px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor"
                                     class="bi bi-people-fill text-purple-800 " viewBox="0 0 16 16">
                                     <path
@@ -96,8 +96,8 @@
         <!-- end -->
         <!-- str -->
         <div class=" grid grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2  bg-blue-400 rounded-lg shadow-md">
-                <div class="flex justify-center items-center w-1 bg-blue-800">
+            <div class="inline-flex overflow-hidden mb-2  bg-purple-800 rounded-lg shadow-md">
+                <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
                     <div class=" grid grid-cols-2 gap-2 w-full">
@@ -106,7 +106,8 @@
                             <p class=" text-purple-800 lg:text-4xl  text-5xl">{{$asrama}}</p>Putra Putri
                         </div>
                         <div class=" grid  grid-cols-1    ">
-                            <div class=" bg-blue-800 lg:px-2 lg:py-1 px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
+                            <div
+                                class=" bg-purple-800 lg:px-2 lg:py-1 px-8   lg:grid-cols-1 sm:grid-cols-1  rounded-lg ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor"
                                     class="bi bi-people-fill text-purple-800 " viewBox="0 0 16 16">
                                     <path
@@ -128,8 +129,8 @@
     <div class=" grid w-full gap-2  md:grid-cols-2 sm:grid-cols-1 sm:text-sm">
         <!-- str -->
         <div class=" grid grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2  bg-gray-150 rounded-lg shadow-md">
-                <div class="flex justify-center items-center w-1 bg-blue-800">
+            <div class="inline-flex overflow-hidden mb-2  bg-purple-100 rounded-lg shadow-md">
+                <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
                     <div class=" grid grid-cols-1 text-2xl gap-2 w-full">
@@ -147,14 +148,14 @@
         <!-- end -->
         <!-- str -->
         <div class=" grid grid-cols-1 w-full">
-            <div class="inline-flex overflow-hidden mb-2  bg-gray-150 rounded-lg shadow-md">
-                <div class="flex justify-center items-center w-1 bg-blue-800">
+            <div class="inline-flex overflow-hidden mb-2  bg-purple-100 rounded-lg shadow-md">
+                <div class="flex justify-center items-center w-1 bg-purple-800">
                 </div>
                 <div class=" w-full  px-2 py-2">
                     <div class=" grid grid-cols-1 text-2xl gap-2 w-full">
                         @foreach($asi as $as)
                         <a href="/asrama/{{ $as->id}}">
-                            <div class="   bg-blue-400 px-2 py-2  text-purple-800 rounded-md">
+                            <div class="   bg-purple-800 px-2 py-2  text-purple-800 rounded-md">
                                 {{ $as->nama_asrama  }} <span class=" float-right">{{$as->hitung}}</span>
                             </div>
                         </a>
