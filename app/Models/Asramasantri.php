830944531c4710977ab031b61bc6634453e11087
sangsakaa <zainuri717@gmail.com>
2022-05-17 19:44:57 +0700

    size

--- app/Models/Asramasantri.php
+++ app/Models/Asramasantri.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Asrama;
 use App\Models\Histori;
+use App\Models\Kelassantri;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
