dc6acc74c3670d82f96bc2aec48a965335093400
sangsakaa <zainuri717@gmail.com>
2022-05-19 17:50:39 +0700

    size

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,25 +4,25 @@
     </x-slot>
     <div class=" grid grid-cols-2 sm:grid-cols-4 w-full gap-2 ">
         <div class=" bg-green-200  rounded-md">
-            <div class=" py-2 px-2">
+            <div class=" py-2 px-4">
                 <span class="font-semibold text-green-700"> Santri Putra</span>
                 <p class=" text-green-800 lg:text-4xl  text-5xl">{{ $l }}</p>Santriwan
             </div>
         </div>
         <div class=" bg-green-200  rounded-md">
-            <div class=" py-2 px-2">
+            <div class=" py-2 px-4">
                 <span class="font-semibold text-green-700"> Santri Putri</span>
                 <p class=" text-green-800 lg:text-4xl  text-5xl">{{ $p }}</p>Santriwan
             </div>
         </div>
         <div class=" bg-green-200  rounded-md">
-            <div class=" py-2 px-2">
+            <div class=" py-2 px-4">
                 <span class="font-semibold text-green-700"> Total Santri</span>
                 <p class=" text-green-800 lg:text-4xl  text-5xl">{{ $putra }}</p>Santriwan
             </div>
         </div>
         <div class=" bg-green-200  rounded-md">
-            <div class=" py-2 px-2">
+            <div class=" py-2 px-4">
                 <span class="font-semibold text-green-700"> Total Asrama</span>
                 <p class=" text-green-800 lg:text-4xl  text-5xl">{{ $asrama }}</p>Putra Putri
             </div>
