7f0330c3b98b95fc8a0c0289289514e5af184269
sangsakaa <zainuri717@gmail.com>
2022-05-21 13:15:46 +0700

    absen new versi

--- resources/views/admin/laporan/report.blade.php
+++ resources/views/admin/laporan/report.blade.php
@@ -34,12 +34,12 @@
                         @endif
                     </div>
                 </div>
-                <div class=" py-2 px-4 border rounded-md grid grid-cols-4">
-                    <div class=" bg-gray-50 px-4 py-1 border">Keterangan</div>
-                    <div class=" bg-gray-50 px-4 py-1 border">Hadir</div>
-                    <div class=" bg-gray-50 px-4 py-1 border">Sakit</div>
-                    <div class=" bg-gray-50 px-4 py-1 border">Alfa</div>
-                    <div class=" px-4 py-1 border">Jumlah</div>
+                <div class=" py-2 px-4  rounded-md grid grid-cols-4">
+                    <div class="font-semibold bg-gray-50 px-4 py-1 border">Keterangan</div>
+                    <div class="font-semibold bg-gray-50 px-4 py-1 border">Hadir</div>
+                    <div class="font-semibold bg-gray-50 px-4 py-1 border">Sakit</div>
+                    <div class="font-semibold bg-gray-50 px-4 py-1 border">Alfa</div>
+                    <div class=" font-semibold px-4 py-1 border">Jumlah</div>
                     <div class=" px-4 py-1 border">{{$jumlahHadir}}</div>
                     <div class=" px-4 py-1 border">{{$jumlahSakit}}</div>
                     <div class=" px-4 py-1 border">{{$jumlahAlfa}}</div>
@@ -47,15 +47,16 @@
                 </div>
             </div>
             <form action="/absen/{{$sesi->id}}" method="post">
-                <button type="submit" class="mt-2 bg-green-700 rounded-md text-white py-1 px-2">save</button>
+                <button type="submit" class="mt-2 bg-green-700 rounded-md text-white py-1 px-2">Simpan Presensi</button>
 
                 @csrf
 
                 <table class="border w-full mb-2 mt-2">
                     <thead>
-                        <tr class="">
+                        <tr class=" uppercase">
                             <th class=" border">#</th>
                             <th class=" px-4 text-left">Nama</th>
+                            <th class=" px-4 border ">Asrama</th>
                             <th class=" px-4 border ">Kelas</th>
                             <th class=" px-4 text-right">Keterangan</th>
                         </tr>
@@ -64,7 +65,8 @@
                         @foreach ($sesi->kelas->kelassantri as $kelassantri)
                         <tr class=" border hover:bg-gray-100">
                             <td class=" border text-center">{{$loop->iteration}}</td>
-                            <td>{{ $kelassantri->asramasantri->santri->nama_santri }}</td>
+                            <td class=" px-2 border">{{ $kelassantri->asramasantri->santri->nama_santri }}</td>
+                            <td class=" text-center border">{{ $kelassantri->asramasantri->asrama->nama_asrama }}</td>
                             <td class=" text-center border">{{ $kelassantri->kelas->nama_kelas }}</td>
                             <td class=" text-right px-2 ">
                                 <input type="radio" id="keteranganhadir[{{ $kelassantri->id }}]"
