c351eb60c4212ae9316453182b3bfb676ca1c082
sangsakaa <zainuri717@gmail.com>
2022-05-25 09:46:44 +0700

    order

--- app/Http/Controllers/KelassantriController.php
+++ app/Http/Controllers/KelassantriController.php
@@ -18,7 +18,7 @@ class KelassantriController extends Controller
     public function index()
 
     {
-        $kelassantri = Asramasantri::orderBy('santri_id');
+        $kelassantri = Asramasantri::orderBy('id', 'asc')->all();
         return view('admin/kelas/listsantri', ['list' => $kelassantri]);
     }
 
