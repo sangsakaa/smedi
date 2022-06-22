9679da1d4fa5d5ce3a80ccd1a17e070c0cc8d6c6
sangsakaa <zainuri717@gmail.com>
2022-06-22 08:41:27 +0700

    new design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,8 +65,8 @@
 
             <div class=" w-full bg-blue-200  px-4   grid grid-cols-2">
                 <div class=" grid content-center">
-                    <span class="font-semibold text-blue-800"> Asrama</span>
-                    <p class=" text-blue-800 lg:text-4xl  text-5xl">{{ $asrama }}</p>Pa :{{ $aslk }} Pi : {{ $aspr }}
+                    <span class="font-semibold text-blue-700"> Asrama</span>
+                    <p class=" text-blue-700 lg:text-4xl  text-5xl">{{ $asrama }}</p>Pa :{{ $aslk }} Pi : {{ $aspr }}
                 </div>
                 <div class=" grid  justify-end content-center  ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="85" height="85" fill="currentColor"
@@ -78,7 +78,30 @@
                     </svg>
                 </div>
             </div>
-
         </div>
     </div>
+    <div class=" grid grid-cols-1 sm:grid-cols-4 gap-2">
+        @foreach($as as $as)
+        <div class="inline-flex overflow-hidden   w-full bg-white  shadow-md">
+            <div class="flex justify-center items-center w-1 bg-blue-800">
+            </div>
+            <div class=" w-full bg-blue-200  px-2   grid grid-cols-2">
+                <div class=" grid content-center text-blue-700">
+                    <span class="font-semibold text-blue-700"> {{ $as->nama_asrama }}</span>
+                    <p class=" text-blue-700 lg:text-xs  text-sm"> Anggota :
+                        {{$as->hitung}} Org
+                    </p>
+                </div>
+                <div class=" grid  justify-end content-center  ">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-people text-blue-700" viewBox="0 0 16 16">
+                        <path
+                            d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
 </x-app-layout>
