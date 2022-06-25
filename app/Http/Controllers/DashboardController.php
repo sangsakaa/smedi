de0ce00239408a3b9b7a306abe8e5cbad70831dd
sangsakaa <zainuri717@gmail.com>
2022-06-25 11:58:08 +0700

    new field rename

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -58,6 +58,6 @@ class DashboardController extends Controller
                 $rekap->where('absensi_kelas.keterangan', '=', request('keterangan'));
             }
         }
-        return view('about', ['rekap' => $rekap->paginate(50), 'asrama' => $asrama, 'kelas' => $kelas]);
+        return view('about', ['rekap' => $rekap->paginate(326), 'asrama' => $asrama, 'kelas' => $kelas]);
     }
 }
