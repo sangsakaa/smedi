bffd47f2c592740dc25f1a29fa9c19e0dc78fb65
sangsakaa <zainuri717@gmail.com>
2022-06-27 10:45:18 +0700

    modifier

--- resources/views/admin/presensi/blangko.blade.php
+++ resources/views/admin/presensi/blangko.blade.php
@@ -52,14 +52,19 @@
         <div class="flex justify-center items-center w-1 bg-green-800">
         </div>
         <div id="div1" class=" w-full px-4 py-2">
-            <div class=" flex w-full">
-                <img src="images/04.jpg" alt="">
+            <div class="  w-full text-center">
+                <!-- <img src="images/04.jpg" alt=""> -->
+                <h1 class=" text-4xl  uppercase text-center py-2 font-semibold text-green-800">madrasah diniyah wustha
+                    wahidiyah</h1>
+                <hr>
+                <h1 class=" text-2xl  capitalize text-center py-2 font-semibold text-green-800">Daftar Harian</h1>
+
             </div>
             <div class=" grid justify-items-end"></div>
             <table class=" w-full">
                 <thead>
                     <tr class=" border">
-                        <th class=" border border-green-800" style="width: {{ 70/($jumlah_hari+1) }}%" rowspan="2">#
+                        <th class=" border border-green-800" style="width: {{ 70/($jumlah_hari+1) }}%" rowspan="2">No
                         </th>
                         <th class=" border border-green-800" class="" style="width: 30%" rowspan="2">Nama</th>
                         <th class=" border border-green-800" colspan="{{ $jumlah_hari }}">
